f2c645db8f646f7808f1ca660a2cba038c50e90e
Devikakrishnan129 <devikakrishnan.ndr@gmail.com>
2025-06-20 19:55:32 +0530

    Delete core directory

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-# prediction_api/urls.py
-
-from django.urls import path
-from .views import SalesPredictionView
-
-urlpatterns = [
-    path('predict_sales/', SalesPredictionView.as_view(), name='predict_sales'),
-]
