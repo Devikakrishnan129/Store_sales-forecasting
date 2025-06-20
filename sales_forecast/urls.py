30e6357a889871d3d6279459f7dd972386ba1430
Devikakrishnan129 <devikakrishnan.ndr@gmail.com>
2025-06-20 19:56:14 +0530

    Delete sales_forecast directory

--- sales_forecast/urls.py
+++
@@ -1,9 +0,0 @@
-# your_project_name/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('core/', include('core.urls')),
-]
